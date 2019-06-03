0a3e09f0bbbc9e4314c23dd6965bdfd7d90aecae
Matthew <matthew.atkinson@protonmail.com>
2019-06-03 18:20:38 +0200

    add index for components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export * from "./__layouts"
+export * from "./_firstPage"
+export * from "./__atoms"
+export * from "./_aboutMe"
+export * from "./_myTimeline"
+export * from "./_mySkills"
+export * from "./__contexts"
