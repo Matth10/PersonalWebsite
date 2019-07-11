0512caebf7d04550002a0e58a82038141d680ceb
Matthew <matthew@linux-1.home>
2019-07-11 16:31:37 +0200

    :bug: fix section height

--- src/components/_firstPage/firstPage.jsx
+++ src/components/_firstPage/firstPage.jsx
@@ -94,7 +94,7 @@ export const FirstPage = () => {
       style={{
         margin: "0 0 15em 0",
         padding: 0,
-        height: windowSize.height,
+        height: window.innerHeight,
       }}
     >
       <ContainerFirstPage>
